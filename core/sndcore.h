d562f140c3d84b21ba62aa3eab26d621600562b2
Chris Fogerty <chfogerty@gmail.com>
2013-02-17 02:26:38 -0600

    Just laying some groundwork.

---
+++ core/sndcore.h
@@ -0,0 +1,8 @@
+//Any generic operation having to do with sound can be found here
+#include "util.h"
+#include "types.h"
+
+//TODO Determine arguments
+snd_t* read_sound();
+void write_sound();
+sndtype_e determine_type();
---
+++ core/types.h
@@ -0,0 +1,18 @@
+enum SoundType {Cs, Wave};
+
+typedef unsigned int u_int;
+typedef unsigned char u_char;
+
+struct Sound
+{
+    u_int rate;
+    u_int num_samples;
+    u_int len;
+    u_char bitdepth;
+    u_char num_channels;
+    enum SoundType type;
+    char* name;
+
+};
+
+typedef struct Sound snd_t;
